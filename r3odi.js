5679058817f992438acdca02958ff5dbb1edd981
r3odi <122447019+r3odi@users.noreply.github.com>
2023-01-11 14:17:42 +0300

    Create r3odi.js

---
+++ r3odi.js
@@ -0,0 +1,9 @@
+$(`<style>.nosel.d-flex.room.r18200a07a4exca08a461x-m77lrn .corner.fa.fa-user.label.uc.fa-microphone.label-danger {
+  letter-spacing: 1px;
+  -webkit-text-fill-color: #000;
+  background-image: url(https://www.jawalchat.net/sico/zbad077cd5b94f97.gif);
+  padding: 0px !important;
+  border-radius: 50px !important;
+}.nosel.d-flex.room.r18200a07a4exca08a461x-m77lrn {display: inline-block;background-image: linear-gradient(-225deg, rgb(255, 250, 202) 0%, rgba(255, 0, 143, 0) 29%, rgba(151, 101, 127, 0.04) 67%, rgb(255, 242, 173) 100%);border-radius: 0px 0px 15px 15px;}
+.nosel.d-flex.room.r18200a07a4exca08a461x-m77lrn .dots.mini.u-msg{text-align:center;background-size:200% auto;-webkit-background-clip:text;-webkit-text-fill-color:#ff9b0000;background-image:-webkit-linear-gradient(75deg,rgb(0 0 0 / 53%) 25%,#ff9b00 25%,#000000 50%,#ff9b00 50%,#ff006600 75%)}.nosel.d-flex.room.r18200a07a4exca08a461x-m77lrn .fitimg.u-pic.borderg{border-radius:100px;border:2px solid #ffcb50;padding-top:30px}.nosel.d-flex.room.r18200a07a4exca08a461x-m77lrn .u-topic.dots{font-weight:900;background-image: linear-gradient(-225deg, #000 0%, #000 29%, #ffb913 67%, #aaa 100%);background-size:300% 300%;-webkit-background-clip:text;-webkit-text-fill-color:#0000;line-height:2;animation:grad 2s cubic-bezier(.445,.05,.55,.95) alternate infinite}@keyframes grad{0%{background-position:14% 0}50%{background-position:87% 100%}100%{background-position:14% 0}
+</style>`).insertBefore('body');
